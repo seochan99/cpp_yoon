0ec3c68e67c8112b7f3bef81fba2141cc50ba25f
seochan99 <gmlcks0513@naver.com>
2021-04-26 22:24:26 +0900

    std::cout

---
+++ 1_practice/1_practice/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  1_practice
+//
+//  Created by 서희찬 on 2021/04/26.
+//
+
+#include <iostream>
+
+int main(void)
+{
+    int num=20;
+    std::cout<<"Hello World!"<<std::endl;
+    std::cout<<"Hello "<<"World!"<<std::endl;
+    std::cout<<num<<' '<<'A';
+    std::cout<<" "<<3.14<<std::endl;
+    return 0;
+}
