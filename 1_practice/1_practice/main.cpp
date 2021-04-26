0a7a7ede6d15eeb114e54cd98d6dada009cfe5de
seochan99 <gmlcks0513@naver.com>
2021-04-26 23:25:53 +0900

    1-1

--- 1_practice/1_practice/main.cpp
+++ 1_practice/1_practice/main.cpp
@@ -7,12 +7,27 @@
 
 #include <iostream>
 
+int CalSalary(int sales)
+{
+    return (int)(50+sales*0.12);
+}
+
 int main(void)
 {
-    int num=20;
-    std::cout<<"Hello World!"<<std::endl;
-    std::cout<<"Hello "<<"World!"<<std::endl;
-    std::cout<<num<<' '<<'A';
-    std::cout<<" "<<3.14<<std::endl;
+    int num=0;
+    
+    while(num!=-1)
+    {
+        std::cout<<"판매 급액을 만원 단위로 입력(-1 to end) : ";
+        std::cin>>num;
+        if(num==-1)
+        {
+            std::cout<<"프로그램을 종료합니다."<<std::endl;
+            break;
+        }
+        std::cout<<"이번 달 급여 : "<<CalSalary(num)<<"만원(만원 단위 출력)"<<std::endl;
+    }
+ 
     return 0;
 }
+
