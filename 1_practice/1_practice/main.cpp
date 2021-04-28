3e5077b4d05e5b1251921dc217282930967f216e
seochan99 <gmlcks0513@naver.com>
2021-04-28 12:35:30 +0900

    4/28

--- 1_practice/1_practice/main.cpp
+++ 1_practice/1_practice/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
-#include "name.hpp"
 
-
+using namespace std;
 int main(void)
 {
-    Best :: SimpleFunc();
-    est :: SimpleFunc();
+    int num2;
+    int num = 20;
+    cout<<"Hello World"<<endl;
+    cout<<"Hello"<<"World"<<endl;
+    cout<<num<<" "<<"A";
+    cout<<" "<<3.14<<endl;
+    cout<<"num2 : ";
+    cin>>num2;
+    
     
     return 0;
 }
-
-
