524822e7288db37327e719ff228fdfa1d6f726cb
seochan99 <gmlcks0513@naver.com>
2021-04-28 10:43:41 +0900

    overloading

--- 1_practice/1_practice/main.cpp
+++ 1_practice/1_practice/main.cpp
@@ -1,33 +1,26 @@
-//
-//  main.cpp
-//  1_practice
-//
-//  Created by 서희찬 on 2021/04/26.
-//
-
 #include <iostream>
 
-int CalSalary(int sales)
+
+void MyFunc(void)
 {
-    return (int)(50+sales*0.12);
+    std::cout<<"MyFunc(void) called"<<std::endl;
+}
+
+void MyFunc(char c)
+{
+    std::cout<<"MyFunc(char c) called"<<std::endl;
+}
+
+
+void MyFunc(int a,int b)
+{
+    std::cout<<"void MyFunc(int a,int b) called"<<std::endl;
 }
 
 int main(void)
 {
-    int num=0;
-    
-    while(num!=-1)
-    {
-        std::cout<<"판매 급액을 만원 단위로 입력(-1 to end) : ";
-        std::cin>>num;
-        if(num==-1)
-        {
-            std::cout<<"프로그램을 종료합니다."<<std::endl;
-            break;
-        }
-        std::cout<<"이번 달 급여 : "<<CalSalary(num)<<"만원(만원 단위 출력)"<<std::endl;
-    }
- 
+    MyFunc();
+    MyFunc('A');
+    MyFunc(12,23);
     return 0;
 }
-
