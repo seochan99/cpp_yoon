4eaa8a3e0777aba636f5ec12224362af697a80f2
seochan99 <gmlcks0513@naver.com>
2021-05-08 22:35:55 +0900

    11

--- 1_practice/1_practice/main.cpp
+++ 1_practice/1_practice/main.cpp
@@ -1,25 +1,15 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-
-void SwapPointer(int *(&ref1), int *(&ref2))
-{
-    int *temp = ref1;
-    ref1 = ref2;
-    ref2 = temp;
-    
-}
 
 int main(void)
 {
-    int num1 = 5;
-    int *ptr1 = &num1;
-    int num2 = 10;
-    int *ptr2 = &num2;
-    SwapPointer(ptr1,ptr2);
-    
-    cout<<*ptr1<<endl;
-    cout<<*ptr2<<endl;
+    srand(time(NULL));
+    for(int i=0;i<5;i++)
+    {
+        printf("Random number #%d : %d\n",i+1,rand()%100);
+    }
     
     return 0;
 }
