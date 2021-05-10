d3644628555c95bf3aac891387bdc76106c36fe9
seochan99 <gmlcks0513@naver.com>
2021-05-10 22:52:26 +0900

    둥

--- Calculator_3/Calculator_3/main.cpp
+++ Calculator_3/Calculator_3/main.cpp
@@ -1,23 +1,69 @@
-//
-//  main.cpp
-//  Calculator_3
-//
-//  Created by 서희찬 on 2021/05/10.
-//
-
 #include <iostream>
-#include "Calculator.hpp"
-
 using namespace std;
+
+class FruiteSeller // 과일판매 아재
+{
+private:
+    int APPLE_PRICE;
+    int numOfApples;
+    int myMoney;
+    
+public:
+    void InitMembers(int price,int num, int money)
+    {
+        APPLE_PRICE = price;
+        numOfApples = num;
+        myMoney = money;
+    }
+    int SaleApples(int money)
+    {
+        int num = money/APPLE_PRICE;
+        numOfApples -= num;
+        myMoney+=money;
+        return num;
+    }
+    void ShowSalesResult()
+    {
+        cout<<"남은 사과 : "<<numOfApples<<endl;
+        cout<<"판매 수익 : "<<myMoney<<endl<<endl;
+    }
+};
+
+class FruiteBuyer
+{
+    int myMoney; // pirvate:
+    int numOfApples; // pricate
+    
+public:
+    void InitMemebrs(int money)
+    {
+        myMoney = money;
+        numOfApples = 0;
+    }
+    void BuyApples(FruiteSeller &seller, int money)
+    {
+        numOfApples += seller.SaleApples(money);
+        myMoney -= money;
+    }
+    void ShowBuyResult()
+    {
+        cout<<"현재 잔액 : "<<myMoney<<endl;
+        cout<<"사과 개수 : "<<numOfApples<<endl<<endl;
+    }
+};
 
 int main(void)
 {
-    Calculator cal;
-    cal.init();
-    cout<<cal.Add(3.2, 2.4)<<endl;
-    cout<<cal.Min(3.2, 2.4)<<endl;
-    cout<<cal.Mul(3.2, 2.4)<<endl;
-    cout<<cal.Div(3.2, 2.4)<<endl;
-    cal.showOpCount();
+    FruiteSeller seller;
+    seller.InitMembers(1000, 20, 0);
+    FruiteBuyer buyer;
+    buyer.InitMemebrs(5000);
+    buyer.BuyApples(seller, 2000); // 과일 구매!
+    
+    cout<<" 과일 판매자의 현황 "<<endl;
+    seller.ShowSalesResult();
+    cout<<"과일 구매자의 현황 "<<endl;
+    buyer.ShowBuyResult();
+    
     return 0;
 }
