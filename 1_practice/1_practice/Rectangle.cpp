e28134964f421a60dcd296b098d673cd50e5aa95
seochan99 <gmlcks0513@naver.com>
2021-05-16 21:44:54 +0900

    33

--- 1_practice/1_practice/Rectangle.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  Rectangle.cpp
-//  1_practice
-//
-//  Created by 서희찬 on 2021/05/12.
-//
-
-#include "Rectangle.hpp"
--- 1_practice/1_practice/main.cpp
+++ 1_practice/1_practice/main.cpp
@@ -1,22 +1,49 @@
-#include "Car.hpp"
+#include <iostream>
 using namespace std;
 
-
-
+class SimpleClass
+{
+private:
+    int num1;
+    int num2;
+public:
+    SimpleClass()//생성자
+    {
+        num1 = 0;
+        num2 = 0;
+    }
+    SimpleClass(int n)
+    {
+        num1 = n;
+        num2 = 0;
+    }
+    SimpleClass(int n1, int n2)
+    {
+        num1 = n1;
+        num2 = n2;
+    }
+//    SimpleClass(int n1 = 0, int n2=0)
+//    {
+//        num1 = n1;
+//        num2 = n2;
+//    }
+    
+    void ShowData() const
+    {
+        cout<<num1<<" "<<num2<<endl;
+    }
+};
 
 int main(void)
 {
-    Car run99;
-    run99.InitMembers("run99", 100);
-    run99.Accel();
-    run99.Accel();
-    run99.Accel();
-    run99.ShowCarState();
-    run99.Break();
-    run99.ShowCarState();
+    SimpleClass sc1;
+    sc1.ShowData();
     
+    SimpleClass sc2(100);
+    sc2.ShowData();
     
-    
+    SimpleClass sc3(100,200);
+    sc3.ShowData();
     
     return 0;
 }
