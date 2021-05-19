c2275478a66e4535f18353017631315c3a738683
seochan99 <gmlcks0513@naver.com>
2021-05-19 21:42:27 +0900

    end

--- BankingSystemVer02/BankingSystemVer02/main.cpp
+++ BankingSystemVer02/BankingSystemVer02/main.cpp
@@ -12,14 +12,55 @@ void ShowAllAccInfo(void); // 잔액조회
 
 enum {MAKE=1, DEPOSIT, WITHDRAW, INQUIRE, EXIT};
 
-typedef struct
+//typedef struct
+//{
+//    int accID; // 계좌번호
+//    int balance; // 잔액
+//    char cusName[NAME_LEN]; //고객 이름 -> 동적할당 형태로 변경
+//}Account;
+class Account
 {
-    int accID; // 계좌번호
-    int balance; // 잔액
-    char cusName[NAME_LEN]; //고객 이름
-}Account;
+private:
+    int accID;
+    int balance;
+    char * cusname;
+public:
+    Account(int accID,int balance,char * name):accID(accID),balance(balance)
+    {
+        cusname = new char[strlen(name)+1];
+        strcpy(cusname, name);
+    }
+    int GetID() // 아이디 얻기
+    {
+        return accID;
+    }
+    void Deposit(int money) // 출 금
+    {
+        balance+=money;
+    }
+    int Withdraw(int money) // 인 출
+    {
+        if(balance<money)
+            return 0;
+        
+        balance-=money;
+        return money;
+    }
+    void ShowInfo() const // 정보
+    {
+        cout<<"계좌 ID : "<<accID<<endl;
+        cout<<"이 름 : "<<cusname<<endl;
+        cout<<"잔 액 : "<<balance<<endl;
+    }
+    ~Account()
+    {
+        delete [] cusname;
+            
+    }
+};
 
-Account accArr[100]; // Account 저장을 위한 배열
+//Account accArr[100]; // Account 저장을 위한 배열 -> 객체 포인터 배열로 선언
+Account * accArr[100];//Account 저장을 위한 포인터 배열
 int accNum = 0; // 저장된 Account 수
 
 int main(void)
@@ -47,6 +88,8 @@ int main(void)
                 ShowAllAccInfo();
                 break;
             case EXIT:
+                for(int i=0;i<accNum;i++)
+                    delete accArr[i]; // 할당해제
                 cout<<"계좌프로그램을 종료합니다."<<endl;
                 return 0;
                 break;
@@ -79,11 +122,12 @@ void MakeAccount(void)
     cout<<"이 름 : "; cin>>name;
     cout<<"입금액 : "; cin>>balance;
     cout<<endl;
-    
-    accArr[accNum].accID=id;
-    accArr[accNum].balance=balance;
-    strcpy(accArr[accNum].cusName, name); //name정보를 배열에 저장
-    accNum++; // 사람 올려 ~
+
+    accArr[accNum++] = new Account(id, balance, name); // 계좌 생성
+//    accArr[accNum].accID=id;
+//    accArr[accNum].balance=balance;
+//    strcpy(accArr[accNum].cusName, name); //name정보를 배열에 저장
+//    accNum++; // 사람 올려 ~
 }
 
 // 입금
@@ -97,10 +141,10 @@ void DepositMoney(void)
     
     for(int i =0;i<accNum;i++)
     {
-        if(accArr[i].accID == id) // 빙빙빙 돌면서 accID를 찾는다
+        if(accArr[i]->GetID() == id) // 빙빙빙 돌면서 accID를 찾는다
         {
-            accArr[i].balance+=money;
-            cout<<"입금완료 ! "<<endl;
+            accArr[i]->Deposit(money);
+            cout<<"입금완료 ! "<<endl<<endl;
             return; // 함수 탈출 !
         }else
             break;
@@ -118,14 +162,13 @@ void WithdrawMoney(void)
     
     for( int i=0;i<accNum;i++)
     {
-        if(accArr[i].accID == id) //아이디와 동일한 계좌 발견
+        if(accArr[i]->GetID() == id) //아이디와 동일한 계좌 발견
         {
-            if(accArr[i].balance<money) // 액수 확인
+            if(accArr[i]->Withdraw(money)==0) // 액수 확인
             {
                 cout<<"잔액부족..."<<endl<<endl;
                 return;
             }
-            accArr[i].balance-=money;
             cout<<"출금 완료"<<endl<<endl;
             return;
         }else
@@ -140,8 +183,7 @@ void ShowAllAccInfo(void)
 {
     for(int i =0;i<accNum;i++)
     {
-        cout<<"계좌 ID : "<<accArr[i].accID<<endl;
-        cout<<"이 름 : "<<accArr[i].cusName<<endl;
-        cout<<"잔 액 : "<<accArr[i].balance<<endl<<endl;
+        accArr[i]->ShowInfo();
+        cout<<endl;
     }
 }
