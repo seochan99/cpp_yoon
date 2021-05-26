d5e658b688ad9f7a772db283a634e5a8b3b86b40
seochan99 <gmlcks0513@naver.com>
2021-05-26 18:28:15 +0900

    ㅎㅎ

--- 1_practice/1_practice/main.cpp
+++ 1_practice/1_practice/main.cpp
@@ -1,37 +1,43 @@
+//#include <iostream>
+//#include <cstring>
+//using namespace std;
+//
+//class Person
+//{
+//private:
+//    char * name;
+//    int age;
+//public:
+//    Person(char * myname,int myage)
+//    {
+//        int len = strlen(myname)+1;
+//        name = new char[len];
+//        strcpy(name, myname);
+//        age = myage;
+//    }
+//    void ShowPersonInfo() const
+//    {
+//        cout<<"이름 : "<<name<<endl;
+//        cout<<"나이 : "<<age<<endl;
+//    }
+//    ~Person()
+//    {
+//        delete [] name;
+//        cout<<"called destructor!"<<endl;
+//    }
+//};
+//
+//int main(void)
+//{
+//    Person man1("hui chan",23);
+//    Person man2("Seo Chan", 41);
+//    man1.ShowPersonInfo();
+//    man2.ShowPersonInfo();
+//    return 0;
+//}
+
 #include <iostream>
-#include <cstring>
-using namespace std;
+sync_with_stdio(false)
+cin.tie(NULL);
 
-class Person
-{
-private:
-    char * name;
-    int age;
-public:
-    Person(char * myname,int myage)
-    {
-        int len = strlen(myname)+1;
-        name = new char[len];
-        strcpy(name, myname);
-        age = myage;
-    }
-    void ShowPersonInfo() const
-    {
-        cout<<"이름 : "<<name<<endl;
-        cout<<"나이 : "<<age<<endl;
-    }
-    ~Person()
-    {
-        delete [] name;
-        cout<<"called destructor!"<<endl;
-    }
-};
 
-int main(void)
-{
-    Person man1("hui chan",23);
-    Person man2("Seo Chan", 41);
-    man1.ShowPersonInfo();
-    man2.ShowPersonInfo();
-    return 0;
-}
--- baekJoon/baekJoon/main.c
+++ baekJoon/baekJoon/main.c
@@ -2,19 +2,13 @@
 
 int main(void)
 {
-    int h,m;
-    scanf("%d %d",&h,&m);
-    m-=45; // 45분 다운
-    if(m<0)
+    int num;
+    scanf("%d",&num);
+    
+    for(int i=num;i>0;i--)
     {
-        h-=1; // 시간 내리기
-        if(h<0)
-        {
-            h=23;
-        }
-        m = 60 + m;
+        printf("%d\n",i);
     }
-    printf("%d %d\n",h,m);
     return 0;
 }
 
