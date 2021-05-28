037406fd01d975d5da4332cb25a7fba39d2b176e
seochan99 <gmlcks0513@naver.com>
2021-05-28 22:31:41 +0900

    dd

--- baekJoon/baekJoon/main.c
+++ baekJoon/baekJoon/main.c
@@ -2,17 +2,9 @@
 
 int main(void)
 {
-    int num1,max=0,maxnum=0;
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
     
-    for(int i=1;i<=9;i++)
-    {
-        scanf("%d",&num1);
-        if(num1>max)
-        {
-            max = num1;
-            maxnum = i;
-        }
-    }
-    printf("%d \n%d",max,maxnum);
+    
     return 0;
 }
