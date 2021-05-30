6991f1b5779f77ec8919ee8bfd2b2d26f6e06a5d
seochan99 <gmlcks0513@naver.com>
2021-05-30 20:05:27 +0900

    22

--- baekJoon/baekJoon/main.c
+++ baekJoon/baekJoon/main.c
@@ -2,9 +2,28 @@
 
 int main(void)
 {
-    int a,b,c;
-    scanf("%d %d %d",&a,&b,&c);
+    int b=0;
+    int arr[10];
     
+    for(int i=0;i<10;i++)
+    {
+        scanf("%d",&arr[i]);
+        arr[i]=arr[i]%42;
+    }
     
+    for(int i=0;i<10;i++)
+    {
+        int a=0; // 매루프마다 0으로 초기화
+        for(int j=0;j<i;j++) // i<j 
+        {
+            if(arr[i]==arr[j])
+                a++;
+        }
+        if(a==0)
+            b++;
+    }
+    
+    printf("%d",b);
     return 0;
 }
+
