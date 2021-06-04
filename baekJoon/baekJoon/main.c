6adabafa29f9a2d987303c639960c03047503e22
seochan99 <gmlcks0513@naver.com>
2021-06-04 21:20:04 +0900

    8

--- baekJoon/baekJoon/main.c
+++ baekJoon/baekJoon/main.c
@@ -2,31 +2,7 @@
 
 int main(void)
 {
-    int num;
-    int o=0,total=0;
-    char arr[80];
-    scanf("%d",&num);
-    
-    for(int i=0;i<num;i++)
-    {
-        scanf("%s",arr[i]);
-    }
-    
-    for(int i=0;i<num;i++)
-    {
-        if(arr[i]=="o")
-        {
-            o++;
-            total+=o;
-        }else if(arr[i]=="x")
-        {
-            o=0;
-        }
-
-    }
-    
-    
-    
+    int total=0,num=0; 
     
     
     return 0;
