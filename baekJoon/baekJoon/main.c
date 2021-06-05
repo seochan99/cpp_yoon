60389d167e18a85f22a7c3448b4ff41cd83b3fee
seochan99 <gmlcks0513@naver.com>
2021-06-05 17:57:12 +0900

    8958

--- baekJoon/baekJoon/main.c
+++ baekJoon/baekJoon/main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    int total=0,num=0; 
+    int num, score;
+    int sum=0;
+    char arr[80];
     
+    scanf("%d", &num);
     
+    for(int i=0;i<num;i++) // 루프문
+    {
+        sum=0;
+        score =1 ;
+        scanf("%s",arr);
+        for(int j=0;j<strlen(arr);j++) // strlen활용 -> 문자열 길이 구하기 가능
+        {
+            if(arr[j]=='O') // 문자 나타낼때는 작은 따음표
+            {
+                sum+=score;
+                score++;
+            }
+            if(arr[j]=='X')
+                score=1; // 스코어 추가하고 아무것도 안더함 
+        }
+        printf("%d\n",sum);
+    }
     return 0;
 }
