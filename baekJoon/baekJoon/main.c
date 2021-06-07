ce7ceae8a265c27a0a81ff996630befa1a857a5f
seochan99 <gmlcks0513@naver.com>
2021-06-07 20:46:39 +0900

    한수

--- baekJoon/baekJoon/main.c
+++ baekJoon/baekJoon/main.c
@@ -1,35 +1,31 @@
 #include <stdio.h>
-//셀프 넘버 = 생성자가 없는 숫자
-int notSelfNumber(int n) //셀프넘어 아닌 숫자 구하는 함수
+// 한수 : 각자릿수가 등차수열을 이룬다
+void HanSoo(int n) //셀프넘어 아닌 숫자 구하는 함수
 {
-    int total = n;
-    
-    while(n>0) //n이 0이하로 가기전 까지 반복
+    int total = 99;
+    int a,b,c;
+    if(n<100)
     {
-        total+=n%10; //나머지
-        n/=10; //나누기
+        printf("%d",n);
+    }else
+    {
+    for(int i=100;i<=n;i++) // 100이하일때는 모두 성립한다.
+    {
+        a=i/100; //100의 자리수
+        b=i/10%10; // 10의 자리수
+        c = i % 10; // 1의 자리수
+        
+        if((b-a)==(c-b))
+            total++;
     }
-    return total;
-    
+        printf("%d",total);
+    }
 }
-
 
 int main(void)
 {
-    int arr[10001], i,check;
-    
-    for(i=1;i<10001;i++)
-    {
-        
-        check = notSelfNumber(i);
-        if(check<10001)
-            arr[check]=1; // 셀프넘버가 아닌 배열의 자리에 1을 저장한다.
-    }
-    for(i=1;i<10001;i++)
-    {
-        if(arr[i]!=1) //셀프넘버 아닌건 배제
-            printf("%d\n",i);
-    }
-    
+    int n;
+    scanf("%d",&n);
+    HanSoo(n);
     return 0;
 }
