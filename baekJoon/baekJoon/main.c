7e37bc92ee4a7956d883f30c097f8608d1a3617d
seochan99 <gmlcks0513@naver.com>
2021-06-08 10:05:53 +0900

    12720

--- baekJoon/baekJoon/main.c
+++ baekJoon/baekJoon/main.c
@@ -1,31 +1,15 @@
 #include <stdio.h>
-// 한수 : 각자릿수가 등차수열을 이룬다
-void HanSoo(int n) //셀프넘어 아닌 숫자 구하는 함수
-{
-    int total = 99;
-    int a,b,c;
-    if(n<100)
-    {
-        printf("%d",n);
-    }else
-    {
-    for(int i=100;i<=n;i++) // 100이하일때는 모두 성립한다.
-    {
-        a=i/100; //100의 자리수
-        b=i/10%10; // 10의 자리수
-        c = i % 10; // 1의 자리수
-        
-        if((b-a)==(c-b))
-            total++;
-    }
-        printf("%d",total);
-    }
-}
 
 int main(void)
 {
-    int n;
+    int n,sum=0;
+    char a[100]={0,};
+
     scanf("%d",&n);
-    HanSoo(n);
+    scanf("%s",a);
+    
+    for(int i=0;i<n;i++)
+        sum+=a[i]-'0'; // 아스키 0 = 48이므로!!!
+    printf("%d\n",sum);
     return 0;
 }
