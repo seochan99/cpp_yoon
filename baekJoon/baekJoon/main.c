ed4c38a3ea1d8c8f1ee4efb77e234a6637e945c6
seochan99 <gmlcks0513@naver.com>
2021-06-10 20:18:22 +0900

    10809
    알파벳찾기

--- baekJoon/baekJoon/main.c
+++ baekJoon/baekJoon/main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    char s[100];
-    scanf("%s",s);
+    int arr[26];
+    char arr2[100];
+    scanf("%s",arr2); // 문자열 입력 받기
+    for(int i=0;i<26;i++) // 모든 배열에 -1저장
+        arr[i]=-1;
     
-    for(int i=0;s[i]!=0;i++)
+    for(int i=0;i<strlen(arr2);i++)
     {
-        printf("%d ",s[i]-97);
+        if(arr[arr2[i]-97]==-1) // 97 = 'a' // 저장된 위치찾음 
+        {
+            arr[arr2[i]-97]=i;
+        }
     }
+    for(int i=0;i<26;i++)
+        printf("%d ",arr[i]);
+    
+    
+    
     return 0;
 }
