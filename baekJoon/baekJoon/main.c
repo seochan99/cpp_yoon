56b55af308f4d682bc9463c8cf1a9c3827df54f0
seochan99 <gmlcks0513@naver.com>
2021-06-10 20:43:22 +0900

    2675

--- baekJoon/baekJoon/main.c
+++ baekJoon/baekJoon/main.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
 #include <string.h>
-
 int main(void)
 {
-    int arr[26];
-    char arr2[100];
-    scanf("%s",arr2); // 문자열 입력 받기
-    for(int i=0;i<26;i++) // 모든 배열에 -1저장
-        arr[i]=-1;
+    int t;
+    char s[100]; // 문자열
     
-    for(int i=0;i<strlen(arr2);i++)
+    scanf("%d",&t); // 테스트케이스 갯수
+    for(int i=0;i<t;i++)
     {
-        if(arr[arr2[i]-97]==-1) // 97 = 'a' // 저장된 위치찾음 
+        int a;
+        scanf("%d %s",&a,s); // 반복횟수
+        for(int j=0;j<strlen(s);j++)
         {
-            arr[arr2[i]-97]=i;
+            for(int k=0;k<a;k++)
+            {
+                printf("%c",s[j]); // 문자 출력 = %c
+            }
         }
+        printf("\n");
     }
-    for(int i=0;i<26;i++)
-        printf("%d ",arr[i]);
-    
-    
-    
     return 0;
 }
