7bf11be11ee3fb3d4b0921a75924900b049833eb
seochan99 <gmlcks0513@naver.com>
2021-06-11 22:28:02 +0900

    222

--- baekJoon/baekJoon/main.c
+++ baekJoon/baekJoon/main.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
 #include <string.h>
+
 int main(void)
 {
-    int t;
-    char s[100]; // 문자열
-    
-    scanf("%d",&t); // 테스트케이스 갯수
-    for(int i=0;i<t;i++)
+    // 문자열 입력
+    char arr[1000000];
+    int arr2[26]={0,};
+    int max,select=0,result=0,i,j;
+    scanf("%s",arr);
+    //대소문자 -> 대문자 통일
+    for(i=0;i<strlen(arr);i++)
     {
-        int a;
-        scanf("%d %s",&a,s); // 반복횟수
-        for(int j=0;j<strlen(s);j++)
-        {
-            for(int k=0;k<a;k++)
-            {
-                printf("%c",s[j]); // 문자 출력 = %c
-            }
-        }
-        printf("\n");
+        if((arr[i]>='a')&&(arr[i]<='z')) // 소문자 -> 대문자 바꿔줌
+            arr[i]-=32;
+        arr2[arr[i]-65]++; //65~90 배열의 자리에 저장
     }
+    // 최댓값 찾기
+    max = arr2[0];
+    for(j=0;j<26;j++)
+    {
+        if(arr2[j]>max)
+        {
+            max=arr2[j];
+            select = j;
+        }
+    }
+    
+    //중복값 찾기
+    for(i=0;i<26;i++)
+    {
+        if(max==arr2[i])
+            result++;
+    }
+    
+    if(result>1)
+        printf("?\n");
+    else
+        printf("%c\n",select+'A');
     return 0;
 }
