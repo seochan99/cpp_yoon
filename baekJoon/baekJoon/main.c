2ad0b1b9ed238ae235a112d3e158659e2ea3ba71
seochan99 <gmlcks0513@naver.com>
2021-06-12 18:33:41 +0900

    1152

--- baekJoon/baekJoon/main.c
+++ baekJoon/baekJoon/main.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
-
-int change(int num)
-{
-    num = ((num%10)*100)+(((num/10)%10)*10)+(num/100);
-    return num;
-}
+#include <string.h>
 
 int main(void)
 {
-    int a,b;
-    scanf("%d %d",&a,&b);
-    a = change(a);
-    b = change(b);
-    if(a>b)
-        printf("%d\n",a);
-    else
-        printf("%d\n",b);
+    char arr[1000000];
+    int i,cnt=0,len;
+    
+    scanf("%[^\n]",arr); // null이 나오기 전까지 읽어버린다 ! = 엔터가 나올때까지 문자열로 받는다 !
+    len = strlen(arr);
+    
+    if(len == 1)
+    {
+        if(arr[0]==' ')
+        {
+            printf("0\n");
+            return 0;
+        }
+    }
+    
+    for(i=1;i<len-1;i++) //마지막은 널문자, i 는 1부터 시작 
+    {
+        if(arr[i]==' ')
+            cnt++; //공백 갯수 세기
+    }
+    printf("%d\n",cnt+1); //공백 + 1 = 단어갯수
     return 0;
 }
