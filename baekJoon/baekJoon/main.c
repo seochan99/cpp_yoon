677bbb6c9c9e9c4ddaadb614cc9c186ab08e90e8
seochan99 <gmlcks0513@naver.com>
2021-06-14 21:54:56 +0900

    22

--- baekJoon/baekJoon/main.c
+++ baekJoon/baekJoon/main.c
@@ -7,10 +7,7 @@ int main(void)
     int i,j,sum;
     scanf("%s",arr);
     
-    sum = strlen(arr); // 문자열 길이만큼이 합
-    // =,- 이 붙으면 한 개 뺀다!
-    // lj, nj -> j와 l,n이 만나면 한 개 뺀다 !
-    // dj= -> dj= 한 개 뺀다 !
+    sum = strlen(arr);
     for(i=0;i<strlen(arr);i++)
     {
         if((arr[i]=='l'||arr[i]=='n')&&arr[i+1]=='j') //lj, nj
