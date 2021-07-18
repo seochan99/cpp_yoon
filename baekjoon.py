c8e3d8be3b2a1ffa5fd285593c381c03626efd25
seochan99 <gmlcks0513@naver.com>
2021-07-18 22:02:50 +0900

    배열마짐가

--- baekjoon.py
+++ baekjoon.py
@@ -1,60 +1,60 @@
-# def d(n):
-#     a = 0
-#     for x in list(str(n)): #i를 받고 리스트만큼 반복  1 5 7 
-#         a = a + int(x)  #각각 더하기 
-#     return int(n) + a #셀프넘버 말고 구하기
+# # def d(n):
+# #     a = 0
+# #     for x in list(str(n)): #i를 받고 리스트만큼 반복  1 5 7 
+# #         a = a + int(x)  #각각 더하기 
+# #     return int(n) + a #셀프넘버 말고 구하기
 
-# a= [] # 배열 선언 
+# # a= [] # 배열 선언 
 
-# for i in range(1,10001):
-#     k = d(i) #i 를 d 함수에 넣기 
-#     a.append(k)
+# # for i in range(1,10001):
+# #     k = d(i) #i 를 d 함수에 넣기 
+# #     a.append(k)
 
-# #a=[1,2,3,4,5,40]
-# for b in range(1, 10001):
-#     if b in a:
-#         pass
-#     else:
-#         print(b) #셀프넘버만 출력 ,생성자가 없는 케이스 
-
-
+# # #a=[1,2,3,4,5,40]
+# # for b in range(1, 10001):
+# #     if b in a:
+# #         pass
+# #     else:
+# #         print(b) #셀프넘버만 출력 ,생성자가 없는 케이스 
 
 
-###
 
 
+# ###
+
+
+# # n = int(input())
+# # han = 0
+# # for i in range(1, n + 1):
+# #     if i < 100: # 100밑으로는 모두 한수 
+# #         han += 1
+# #     else:
+# #         ns = list(map(int, str(i)))
+# #         if ns[0] - ns[1] == ns[1] - ns[2]: # 123  ns[0] = 1, ns[1] = 2, ns[2] = 3 
+# #             han += 1
+# # print(han)
+
+# # 벌집 
+
+# n = int(input()) # 입력받기 
+
+# num_bulzip = 1  #벌집개수
+# cnt = 1
+
+# while n > num_bulzip :
+#     num_bulzip += 6 * cnt  # 벌집이 6의 배수로 증가
+#     cnt += 1  
+# print(cnt) #총횟수 출력
+
+
+
+# #호텔
 # n = int(input())
-# han = 0
-# for i in range(1, n + 1):
-#     if i < 100: # 100밑으로는 모두 한수 
-#         han += 1
-#     else:
-#         ns = list(map(int, str(i)))
-#         if ns[0] - ns[1] == ns[1] - ns[2]: # 123  ns[0] = 1, ns[1] = 2, ns[2] = 3 
-#             han += 1
-# print(han)
-
-# 벌집 
-
-n = int(input()) # 입력받기 
-
-num_bulzip = 1  #벌집개수
-cnt = 1
-
-while n > num_bulzip :
-    num_bulzip += 6 * cnt  # 벌집이 6의 배수로 증가
-    cnt += 1  
-print(cnt) #총횟수 출력
-
-
-
-#호텔
-n = int(input())
-for _ in range(int(input())):
-    h,w,p = map(int,input().split())
-    a = n % h ; b = n//h+1
-    if a==0 : a=h ; b-=1
-    print(a*100+b)
+# for _ in range(int(input())):
+#     h,w,p = map(int,input().split())
+#     a = n % h ; b = n//h+1
+#     if a==0 : a=h ; b-=1
+#     print(a*100+b)
 
 
 
@@ -62,16 +62,16 @@ for _ in range(int(input())):
 
 
 
-# #달팽이
-a, b, v = map(int, input().split())
-day = 0
-#0이 아니라면 아직 도착한것이 아니다 ! +1 해준다 
-if (v - b) % (a - b) != 0: #목표지점도달시 미끌x => v-b만큼의 높이를 올라가면된다. 
-    day = ((v - b) // (a - b)) + 1
-#0이라면 몫출력한다 
-else:
-    day = ((v - b) // (a - b)) # a-b만큼올라간다 하루마다 
-print(day)
+# # #달팽이
+# a, b, v = map(int, input().split())
+# day = 0
+# #0이 아니라면 아직 도착한것이 아니다 ! +1 해준다 
+# if (v - b) % (a - b) != 0: #목표지점도달시 미끌x => v-b만큼의 높이를 올라가면된다. 
+#     day = ((v - b) // (a - b)) + 1
+# #0이라면 몫출력한다 
+# else:
+#     day = ((v - b) // (a - b)) # a-b만큼올라간다 하루마다 
+# print(day)
 
 
 
@@ -82,14 +82,30 @@ print(day)
 
 
 
-#부녀회 
-T = int(input()) #테스트케이스 받기 
+# #부녀회 
+# T = int(input()) #테스트케이스 받기 
 
-for _ in range(T): #테스트케이스만큼 반복 
-    k = int(input()) #k층
-    n = int(input()) #n호
-    people = [ i for i in range(1, n+1)]  #리스트 포문작성[표현식 for 항목 in 리스트(range)] [0,1,2....]
-    for __ in range(k): #층만큼
-        for j in range(1,n): #호만큼
-            people[j] += people[j-1] 
-    print(people[-1]) #오른쪽 끝 요소 출력 
+# for _ in range(T): #테스트케이스만큼 반복 
+#     k = int(input()) #k층
+#     n = int(input()) #n호
+#     people = [ i for i in range(1, n+1)]  #리스트 포문작성[표현식 for 항목 in 리스트(range)] [0,1,2....]
+#     for __ in range(k): #층만큼
+#         for j in range(1,n): #호만큼
+#             people[j] += people[j-1] 
+#     print(people[-1]) #오른쪽 끝 요소 출력 
+
+
+n=int(input())
+
+ans=0
+for i in range(n): #n만큼 반복 
+    word = input() #단어 입력받음 
+    for j in range(len(word)):
+        if j!=len(word)-1: #단어의 끝도달전
+            if word[j]==word[j+1]: #옆에 같은 문자 있을 시 패스 
+                pass
+            elif word[j] in word[j+1:]: #옆이 아닌 다른곳에 있을 시 브레이크 
+                break 
+        else:
+            ans +=1 # 브레이크없이 끝날경우 ans +=1 진행
+print(ans)
